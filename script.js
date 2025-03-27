df44b5d5a81e2be7482459bc4327e9513f5cf90c
hadinotknown <hadinotknown@gmail.com>
2025-03-27 19:18:58 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Example: Upcoming show info
+    document.getElementById("shows").innerText = "April 20, 2025 - Boston, MA";
+
+    // Example: Latest release info
+    document.getElementById("news").innerText = "New single 'Echo Chamber' out now!";
+});
