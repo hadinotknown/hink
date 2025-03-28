c4fb3763ecc40dec73c72b63cc1845b4ff310222
hadinotknown <hadinotknown@gmail.com>
2025-03-27 20:09:28 -0400

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Example: Upcoming show info
-    document.getElementById("shows").innerText = "April 20, 2025 - Boston, MA";
+    document.getElementById("shows").innerText = "04/05 @ The Valley";
 
     // Example: Latest release info
-    document.getElementById("news").innerText = "New single 'Echo Chamber' out now!";
+    document.getElementById("news").innerText = "Restart/All You Can Do is Laugh + Had I Not Known I Was Already Dead I Would Mourn My Loss of Life";
 });
